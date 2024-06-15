7f17cd95f525f32cde326a189bbc076447ab7bd6
Nhi Nguyen <nvnhi1811@gmail.com>
2024-06-15 18:28:12 +0900

    Revert the change to check if github action PASS

--- src/unit_tests/utils.test.ts
+++ src/unit_tests/utils.test.ts
@@ -1,7 +1,7 @@
 import add from '../utils/utils'
 
 describe('testing add function', () => {
-    test('empty string should result in zero', () => {
+    test('1 + 2 should equal 3', () => {
       expect(add(1, 2)).toBe(3);
     });
   });
